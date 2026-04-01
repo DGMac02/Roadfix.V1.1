dd2b181b2fba488b124b70b72a3edb62fd5b4b0a
DGMac02 <macheke519@gmail.com>
2026-04-01 10:10:18 +0200

    commit

--- RoadFix/obj/Debug/net9.0/RoadFix.AssemblyInfo.cs
+++ RoadFix/obj/Debug/net9.0/RoadFix.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoadFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad2646bc615d50695dd4639ecfb0a10368b6392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456dd42ecf43d0157c993d30cbfd570b259e56a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
